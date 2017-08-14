6e1e4eb390bd27de7681c87035387776ec42a242
macro6461 <mattcroak718@gmail.com>
2017-08-14 14:21:03 -0400

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, 'Mr.Chocolate', 3, "M", 'brown', 'nice', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, 'Rowdy', 6, "M", 'black', 'rowdy', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, 'Tabitha', 2, "F", 'brown', 'nice', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, 'Sergeant Brown', 7, "M", 'black', 'grumpy', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, 'Melissa', 3, "F", 'brown', 'nice', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, 'Grinch', 3, "M", 'grey', 'nice', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, 'Wendy', 3, "F", 'white', 'mean', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 3, 'M', 'brown', 'aggressive', 1);
